6e607dc61971d351bf29b4f3dba4f80489618416
JnuLuX <landstar@126.com>
2018-09-06 12:34:15 +0800

    增加通用性
    修复field不一致问题

--- net_check.sh
+++ net_check.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -
-command=`ping -c 10 www.baidu.com | grep packets | awk '{print $7}'|awk -F '%' '{print $1}'`
+#command=`ping -c 10 www.baidu.com | grep packets | awk '{print $7}'|awk -F '%' '{print $1}'`
+command=`ping -c 10 www.baidu.com | grep packets | awk -F '%' '{print $1}' | awk -F ',' '{print $NF}'
 if [ $command -gt 40 ]
 then
     echo "${command}% packets lost, restart mentohust"
